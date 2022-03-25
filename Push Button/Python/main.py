bc0c93e7c9c19464ef629ea4d6c9f3b6f0366610
samanazadi1996@gmail.com <samanazadi1996@gmail.com>
2022-03-25 04:38:50 +0430

    add c# project in Blinking LED

--- Push Button/Python/main.py
+++ Push Button/Python/main.py
@@ -3,13 +3,11 @@ import RPi.GPIO as GPIO
 
 def button_callback(channel):
     print("Button was pushed!")
-
-
-GPIO.setwarnings(False)  # Ignore warning for now
-GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
-# Set pin 10 to be an input pin and set initial value to be pulled low (off)
+    
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-# Setup event on pin 10 rising edge
 GPIO.add_event_detect(10, GPIO.RISING, callback=button_callback)
+
 message = input("Press enter to quit\n\n")  # Run until someone presses enter
-GPIO.cleanup()  # Clean up
+GPIO.cleanup()
