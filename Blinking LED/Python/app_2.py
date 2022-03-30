109de2ca8d7d1b9e87b0aaf4d0bedee7f60cbf5f
samanazadi1996@gmail.com <samanazadi1996@gmail.com>
2022-03-31 02:45:12 +0430

    Create Blinking LED app_2

---
+++ Blinking LED/Python/app_2.py
@@ -0,0 +1,24 @@
+import RPi.GPIO as GPIO
+import time
+
+pin = 7
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(pin, GPIO.OUT)
+
+step = 10
+light = 10
+
+p = GPIO.PWM(pin, 100)
+p.start(light)
+
+try:
+    while True:
+        if light in [0, 100]:
+            step = step * (-1)
+        light += step
+        p.ChangeDutyCycle(light)
+        time.sleep(1)
+finally:
+    p.stop()
+    GPIO.cleanup()
